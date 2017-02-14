536b54b3acd3b290c83ac8f2c585093b35e053fe
taran1310 <taran1310@gmail.com>
2017-02-14 11:11:35 +0530

    Add files via upload

---
+++ views/controller/loginPage.js
@@ -0,0 +1,13 @@
+ var myController=function ($scope, $http)
+ {
+   
+    $scope.data = [];
+    var request = $http.get('/data');    
+    request.success(function(data) {
+        $scope.data = data;
+    });
+    request.error(function(data){
+        console.log('Error: ' + data);
+    });
+
+     }
