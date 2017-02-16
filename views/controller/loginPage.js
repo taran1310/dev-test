790fd9e8a44a141df70deb83998173e084f98bed
taran1310 <taran1310@gmail.com>
2017-02-16 18:02:16 +0530

    Update loginPage.js

--- views/controller/loginPage.js
+++ views/controller/loginPage.js
@@ -9,5 +9,69 @@
     request.error(function(data){
         console.log('Error: ' + data);
     });
+     
+     
+     
+      var canvas = document.getElementById('canvas');
+    var con = canvas.getContext('2d');
+    
+    var chars = "abcdefghijklmnopqrstuvwxyz1234567890#!$";
+    $scope.text = "";
+    
+    //n.font = "30pt couriernew";  
+    con.font = "20pt couriernew"; 
+    var x = 20
+    var y = 50;
+    
+    for(i=0; i<5; i++) {
+        var r = Math.floor(Math.random() * chars.length) ;
+        var c = Math.round(Math.random());
+        if(c == 1 && r < 24){
+            $scope.text = chars[r].toUpperCase();
+            con.fillText($scope.text, x, y);
+            x += 30;
+            y += 0;
+        } else {
+            $scope.text = chars[r];
+            con.fillText($scope.text, x, y);
+            x += 30;
+            y += 0;
+        }
+    } 
+     
+     
+     $scope.refreshCaptcha=function()
+     {
+         var canvas = document.getElementById('canvas');
+        var con = canvas.getContext('2d');
+         
+         con.clearRect(0, 0, canvas.width, canvas.height);
+    
+    var chars = "abcdefghijklmnopqrstuvwxyz1234567890#!$";
+     $scope.text = "";
+    
+    //n.font = "30pt couriernew";  
+    con.font = "20pt couriernew"; 
+    var x = 20
+    var y = 50;
+    
+    for(i=0; i<5; i++) {
+        var r = Math.floor(Math.random() * chars.length) ;
+        var c = Math.round(Math.random());
+        if(c == 1 && r < 24){
+            $scope.text = chars[r].toUpperCase();
+            con.fillText($scope.text, x, y);
+            x += 30;
+            y += 0;
+        } else {
+            $scope.text = chars[r];
+            con.fillText($scope.text, x, y);
+            x += 30;
+            y += 0;
+        }
+    }  
+     }
+     
+     
 
      }
